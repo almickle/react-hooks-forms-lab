6c1b6b463d45488d421cb934caeece8a7983a52c
Michael Dear <michaelcdear@gmail.com>
2022-07-25 14:51:55 -0600

    commit tree 56d403deb441adbfd68bb44f7a068c9ae53e4cfd

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -2,31 +2,72 @@ import React, { useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
+import { v4 as uuid } from "uuid";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchInput, setSearchInput] = useState("")
+  const [newName, setNewName] = useState()
+  const [newCategory, setNewCategory] = useState("Produce")
+  const [newItem, setNewItem] = useState()
+  const [displayItems, setNewArray] = useState(items)
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
+  function handleSearch(event) {
+    setSearchInput(event.target.value)
+  }
+
+  // Add new item handling
+  function handleNewName(event) {
+    event.preventDefault()
+    setNewName(event.target.value)
+  }
+
+  function handleNewCategory(event) {
+    setNewCategory(event.target.value)
+  }
+
+  function onItemFormSubmit(event) {
+      event.preventDefault()
+        const newFormItem = {
+          id: uuid(),
+          name: newName,
+          category: newCategory,
+        }
+      setNewItem(newFormItem)
+      setNewArray([...items, newFormItem])
+      console.log("displayItems")
+      console.log(displayItems)
+  }
+
+
+  const itemsToDisplay = displayItems.filter((item) => {
+    if (searchInput !== "") return item.name.toLowerCase().includes(searchInput.toLowerCase())
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
   });
 
+  const displayElements = itemsToDisplay.map(item => <Item key={item.id} name={item.name} category={item.category} />)
+
+
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm assignName={handleNewName} assignCategory={handleNewCategory} onItemFormSubmit={onItemFormSubmit} />
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={handleSearch} />
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+       {displayElements}
       </ul>
     </div>
   );
 }
 
 export default ShoppingList;
+
+
+// {itemsToDisplay.map((item) => (
+//   <Item key={item.id} name={item.name} category={item.category} />
+// ))}
